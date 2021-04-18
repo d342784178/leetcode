a91ac4209032ac34cea251c0c2fcab6ccfc224b7
DLJ <ljdong2@ifleweb.com>
2021-04-18 22:09:11 +0800

    补充注释

--- leetcode/editor/cn/[76]最小覆盖子串.java
+++ leetcode/editor/cn/[76]最小覆盖子串.java
@@ -6,9 +6,12 @@ import java.util.Map;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String minWindow(String s, String t) {
+        //思路: 1. 收集s中元素所在位置,统计t中各元素个数
+        //2.滑动窗口[i,j]遍历s,每次指针前进都计算[i,j]还欠缺多少元素
         HashMap<Character, List<Integer>> aa = aa(s);
         HashMap<Character, Integer>       bb = bb(t);
-        int                               i  = 0, ii = 0, jj = s.length();
+        //i:i指针 ii:ii指针 jj:长度
+        int i = 0, ii = 0, jj = s.length();
         for (; i < s.length(); ) {
             int j = i + t.length() - 1;
             //j往前走直到contain(t)
@@ -16,20 +19,25 @@ class Solution {
                 Integer less = contain(i, j, aa, bb);
                 //System.out.println(less + "   " + i + "," + j + " " + s.substring(i, j + 1));
                 if (less == 0) {
+                    //[i,j]中包含t中所有元素
                     if (j - i < jj - ii) {
+                        //当前窗口小于上一最小窗口,更新窗口
                         ii = i;
                         jj = j;
                         //System.out.println("contain: " + s.substring(ii, jj + 1));
                     }
                     break;
                 } else {
+                    //当前[i,j]还欠缺t中less个元素,因此窗口直接扩大less
                     j += less;
                 }
             }
+
             if (j >= s.length()) {
+                //s不包含t中所有元素,直接跳出
                 break;
             }
-
+            //i往前缩小敞口
             while (i > j - t.length()) {
                 //固定j,i往前走缩小范围
                 Integer less = contain(i, j, aa, bb);
@@ -42,6 +50,7 @@ class Solution {
                     }
                     i++;
                 } else {
+                    //一旦t往前导致less>0,则直接跳出
                     break;
                 }
             }
@@ -56,31 +65,39 @@ class Solution {
 
 
     /**
-     * 优化: 参考leetcode标准答案
+     * TODO 优化: 参考leetcode标准答案
      * @param i
      * @param j
      * @param aa
      * @param bb
-     * @return
+     * @return [i,j]还欠缺多少bb元素个数
      */
     private Integer contain(int i, int j, Map<Character, List<Integer>> aa, Map<Character, Integer> bb) {
         int less = 0;
         for (Map.Entry<Character, Integer> entry : bb.entrySet()) {
             List<Integer> integers = aa.get(entry.getKey());
+            //bb元素在aa中是否存在
             if (integers == null) {
+                //不存在,则将bb次数加到less中
                 less += entry.getValue();
                 continue;
             }
+            //存在,统计在[i,j]中存在多少该元素
             int contains = (int) integers.stream().filter(integer -> integer >= i && integer <= j).count();
             if (contains >= entry.getValue()) {
+                //数量大于bb中该元素所需的数量,则continue
                 continue;
             } else {
+                //否则,将欠缺数量加到less中
                 less += entry.getValue() - contains;
             }
         }
         return less;
     }
 
+    /**
+     * 以key:字符维度整合index
+     */
     private HashMap<Character, List<Integer>> aa(String s) {
         HashMap<Character, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
@@ -93,6 +110,9 @@ class Solution {
         return map;
     }
 
+    /**
+     * 以key:字符纬度统计出现次数
+     */
     private HashMap<Character, Integer> bb(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
